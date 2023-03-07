8588a4f4d176bbbb0389e1ba48bdc361c5354fa3
rifaturrana <smrifaturrana@gmail.com>
2023-03-08 00:34:25 +0600

    getStaticPaths

--- src/pages/blog/[blogId].jsx
+++ src/pages/blog/[blogId].jsx
@@ -1,14 +1,41 @@
-import {useRouter} from 'next/router'
+import { useRouter } from "next/router";
 
-const BlogDetails = ()=>{
-    const router = useRouter()
-    const {blogId} = router.query
-    console.log(blogId);
-    return(
-        <div>
-            <h1>Blog Details of {blogId}</h1>
-        </div>
-    )
+const BlogDetails = (props) => {
+  const router = useRouter();
+  const { blogId } = router.query;
+  console.log(blogId);
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h1>{props.post.title}</h1>
+    </div>
+  );
+};
+export async function getStaticProps(context) {
+  const { params } = context;
+  console.log(params);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.blogId}`
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { blogId: "1" } },
+      { params: { blogId: "2" } },
+      { params: { blogId: "3" } },
+    ],
+    fallback: true,
+  };
 }
 
-export default BlogDetails
+export default BlogDetails;
