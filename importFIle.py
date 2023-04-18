25bb084d1fa641a679b55e4c742e72cad98c603f
rifaturrana <smrifaturrana@gmail.com>
2023-04-19 00:51:12 +0600

    OOP in py

---
+++ importFIle.py
@@ -0,0 +1,8 @@
+class Teacher:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def printInfo(self):
+        print("Name: ", self.name)
+        print("Age: ", self.age)
